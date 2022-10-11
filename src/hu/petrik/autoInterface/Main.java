bc8d603e9bcfc6c59c206c936ae065b313a43bd9
Maurisz <p2002adam@gmail.com>
2022-10-11 15:58:29 +0200

    Autok kesz egesz

--- src/hu/petrik/autoInterface/Main.java
+++ src/hu/petrik/autoInterface/Main.java
@@ -1,8 +1,29 @@
 package hu.petrik.autoInterface;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+    static List<Auto> autok = new ArrayList<>();
+
     public static void main(String[] args) {
-	// write your code here
+
+        Porsche p1 = new Porsche(120);
+        Porsche p2 = new Porsche(150);
+        Porsche p3 = new Porsche(100);
+        Tesla t1 = new Tesla();
+        Tesla t2 = new Tesla();
+
+        autok.add(p1);
+        autok.add(p2);
+        autok.add(p3);
+        autok.add(t1);
+        autok.add(t2);
+
+        for (Auto a : autok){
+            System.out.println(a.getSebesseg());
+        }
+
     }
 }
